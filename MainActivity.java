a9ce9405de62f92e85d0f1bbcdf1193f4169e55d
Estephany <58555141+es12pha27@users.noreply.github.com>
2021-11-04 15:41:21 -0500

    modificacion tepha

--- MainActivity.java
+++ MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
-
+//Estephany modifico esto 
 public class MainActivity extends AppCompatActivity {
     private SharePreferenceHandler sharePreferenceHandler;
     private EditText n_emp;
